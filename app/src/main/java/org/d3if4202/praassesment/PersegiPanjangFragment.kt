c4ad73e901ce3cf0f54e4b7a982f7775a48b588b
Ridloyanuar <Ridloyanuar@gmail.com>
2020-03-02 09:09:35 +0700

    initial commit

--- app/src/main/java/org/d3if4202/praassesment/PersegiPanjangFragment.kt
+++ app/src/main/java/org/d3if4202/praassesment/PersegiPanjangFragment.kt
@@ -1,6 +1,9 @@
 package org.d3if4202.praassesment
 
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import androidx.fragment.app.Fragment
@@ -14,6 +17,8 @@ import org.d3if4202.praassesment.databinding.FragmentPersegiPanjangBinding
 
 
 class PersegiPanjangFragment : Fragment() {
+
+    public lateinit var packageManager: PackageManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentPersegiPanjangBinding>(
@@ -33,8 +38,29 @@ class PersegiPanjangFragment : Fragment() {
             txt_hasil_keliling_pp.text = keliling.toString()
         }
 
+        binding.btnShareSegitiga.setOnClickListener {
+            val subject = "Perhitungan Persegi Panjang"
+            val message = "Luas Persegi Panjang = " + txt_hasil_luas_pp.text + "\n " + "" +
+                    "Keliling Persegi Panjang = " +txt_hasil_keliling_pp.text
+            val addresses = "Ridloyanuar@gmail.com"
+            composeEmail(addresses,subject,message)
+        }
+
         return binding.root
     }
+
+    fun composeEmail(addresses: String, subject: String, message: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(addresses))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+            startActivity(intent)
+
+    }
+
+
 
 
 }
--- app/src/main/java/org/d3if4202/praassesment/SegitigaFragment.kt
+++ app/src/main/java/org/d3if4202/praassesment/SegitigaFragment.kt
@@ -1,12 +1,15 @@
 package org.d3if4202.praassesment
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_persegi_panjang.*
 import kotlinx.android.synthetic.main.fragment_segitiga.*
 import org.d3if4202.praassesment.databinding.FragmentFirstBinding
 import org.d3if4202.praassesment.databinding.FragmentSegitigaBinding
@@ -33,8 +36,26 @@ class SegitigaFragment : Fragment() {
             txt_hasil_keliling_segitiga.text = keliling.toString()
         }
 
+        binding.btnShareSegitiga.setOnClickListener {
+            val subject = "Perhitungan Segitiga"
+            val message = "Luas Segitiga = " + txt_hasil_luas_segitiga.text + "\n " + "" +
+                    "Keliling Segitiga = " +txt_hasil_keliling_segitiga.text
+            val addresses = "Ridloyanuar@gmail.com"
+            composeEmail(addresses,subject,message)
+        }
+
 
         return binding.root
+    }
+
+     fun composeEmail(addresses: String, subject: String, message: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(addresses))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+        startActivity(intent)
     }
 
 
